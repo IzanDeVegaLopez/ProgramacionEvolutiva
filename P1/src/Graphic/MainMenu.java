e2b67c102a90df0b33d0ee72bce679b805c55a56
Eric11195 <izandeve@ucm.es>
2026-02-09 10:15:58 +0100

    paneles de selección añadidos

--- P1/src/Graphic/MainMenu.java
+++ P1/src/Graphic/MainMenu.java
@@ -8,6 +8,10 @@ import java.lang.*;
 import org.math.plot.*;
 
 public class MainMenu extends MyFrame{
+    int boxSizeY = 150;
+    int labelSizeX = 460;
+    int menuDesplegableSizeX = 300;
+
     public MainMenu(int x,int y){
         super(x,y);
 
@@ -30,6 +34,8 @@ public class MainMenu extends MyFrame{
         pan.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
         pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
 
+        pan.add(createAllMenusDesplegables());
+
         pan.add(createCheckBox("Elitism"));
         pan.add(createCheckBox("second checkbox"));
         pan.add(createCheckBox("third checkbox"));
@@ -38,11 +44,54 @@ public class MainMenu extends MyFrame{
         return pan;
     }
 
+    JPanel createAllMenusDesplegables(){
+        MyPanel panelConjunto = new MyPanel(150,150,150);
+        panelConjunto.setLayout(new BoxLayout(panelConjunto, BoxLayout.X_AXIS));
+        panelConjunto.setMaximumSize(new Dimension(labelSizeX + menuDesplegableSizeX, boxSizeY));
+        panelConjunto.setMinimumSize(new Dimension(labelSizeX + menuDesplegableSizeX, boxSizeY));
+
+        MyPanel panL = new MyPanel(255,255,255);
+        MyPanel panR = new MyPanel(255,255,255);
+        panL.setLayout(new BoxLayout(panL, BoxLayout.Y_AXIS));
+        panR.setLayout(new BoxLayout(panR, BoxLayout.Y_AXIS));
+
+        //Codificación
+        panL.add(createLabel("Codificación"));
+        panR.add(createMenuDesplegable(new String[]{"Binario", "Punto flotante"}));
+        //método de Selección
+        panL.add(createLabel("Método de Selección"));
+        panR.add(createMenuDesplegable(new String[]{"Ruleta", "Torneo", "Estocástico", "Truncamiento", "Restos"}));
+        //Operadores de cruce
+        panL.add(createLabel("Operadores de cruce"));
+        panR.add(createMenuDesplegable(new String[]{"Monopunto", "Uniforme"}));
+        //Mutación
+        panL.add(createLabel("Mutación"));
+        panR.add(createMenuDesplegable(new String[]{"A nivel de bit"}));
+
+        panelConjunto.add(panL);
+        panelConjunto.add(panR);
+        return panelConjunto;
+    }
+
     Checkbox createCheckBox(String s){
         Checkbox chckBx = new Checkbox(s);
         chckBx.setSize(30,40);
         chckBx.setBackground(new Color(0,255,255));
         return chckBx;
+    }
+
+    JLabel createLabel(String s){
+        JLabel l = new JLabel(s);
+        l.setMaximumSize(new Dimension(labelSizeX, boxSizeY));
+        l.setMinimumSize(new Dimension(labelSizeX, boxSizeY));
+        return l;
+    }
+
+    JComboBox createMenuDesplegable(String[] s){
+        JComboBox cmb = new JComboBox<>(s);
+        cmb.setMaximumSize(new Dimension(menuDesplegableSizeX, boxSizeY));
+        cmb.setMinimumSize(new Dimension(menuDesplegableSizeX, boxSizeY));
+        return cmb;
     }
 
     Plot2DPanel createGraphicsMenu(){
