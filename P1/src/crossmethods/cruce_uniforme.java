ecff1ba3685eed1b4f78a25ede3d6940e299edda
Eric11195 <izandeve@ucm.es>
2026-02-09 12:24:27 +0100

    name fix

--- P1/src/crossmethods/cruce_uniforme.java
+++ P1/src/crossmethods/cruce_uniforme.java
@@ -3,7 +3,7 @@ package crossmethods;
 import codification.codificacion_binaria;
 import codification.codificacion_real;
 
-public class cruce_multipunto implements bin_cross_base, float_cross_base{
+public class cruce_uniforme implements bin_cross_base, float_cross_base{
     public float probability = 0.3f;
     public void cross(codificacion_real cod, int elemIdxA, int elemIdxB){
         for(int i = 0; i < cod.getNElems(); ++i) {
