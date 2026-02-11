1cce75e0172e2ad93d362d94eb236a5c00676e3a
Óscar Melquiades Durán Narganes <149335285+Melduna@users.noreply.github.com>
2026-02-11 14:11:58 +0100

    Update estocastico.java

--- P1/src/selection_methods/estocastico.java
+++ P1/src/selection_methods/estocastico.java
@@ -1,4 +1,17 @@
 package selection_methods;
 
-public class estocastico {
+public class estocastico implements selection_method{
+    public int[] chooseEntities(int[] fitness){
+        int[] selected = new int[fitness.length];
+        tabla_frecuencias t = new tabla_frecuencias(fitness);
+        float separacion = 1.0f / fitness.length;
+        float marca = (float)Math.random()*separacion;
+        int index = 0;
+        for (int i = 0; i<fitness.length;i++){
+            while (t.frec_rel_acumulada[index] < marca) index++;
+            selected[i] = index;
+            marca += separacion;
+        }
+        return selected;
+    }
 }
