8141091e69dbaad80d9423aee35bfd3107960a0f
Eric11195 <119250069+Eric11195@users.noreply.github.com>
2026-03-06 17:32:13 +0100

    CO_cross

--- P1/src/crossmethods/co_cross.java
+++ P1/src/crossmethods/co_cross.java
@@ -2,8 +2,62 @@ package crossmethods;
 
 import codification.codificacion_entera;
 
-public class co_cross implements base_cross_method{
-    public void cruzar(codificacion_entera element1, codificacion_entera element2) {
+import java.util.BitSet;
 
+public class co_cross implements base_cross_method{
+    public void cruzar(codificacion_entera padre1, codificacion_entera padre2) {
+        int punto_corte = (int) (Math.random() * padre1.get_size());
+
+        int[] ordinal_padre1 = convertir_a_ordinal(padre1);
+        int[] ordinal_padre2 = convertir_a_ordinal(padre2);
+
+        int[] ordinal_hijo1 = new int[padre1.get_size()];
+        int[] ordinal_hijo2 = new int[padre2.get_size()];
+
+        for(int i = 0; i < punto_corte; ++i){
+            ordinal_hijo1[i] = ordinal_padre1[i];
+            ordinal_hijo2[i] = ordinal_padre2[i];
+        }
+        for(int i = punto_corte; i < ordinal_hijo1.length; ++i){
+            ordinal_hijo1[i] = ordinal_padre2[i];
+            ordinal_hijo2[i] = ordinal_padre1[i];
+        }
+
+        padre1.copy(convertir_a_codificacion(ordinal_hijo1));
+        padre2.copy(convertir_a_codificacion(ordinal_hijo2));
+    }
+
+    public int[] convertir_a_ordinal(codificacion_entera cod){
+        BitSet b = new BitSet(cod.get_size());
+        b.clear();
+
+        int[] result = new int[cod.get_size()];
+        for(int i = 0; i < cod.get_size(); ++i){
+            int idx = 0;
+            int penalizacion = 0;
+            int value_to_reach = cod.get_value(i);
+            //searches the next element
+            while((idx = b.nextSetBit(idx)) < value_to_reach) {
+                ++idx; ++penalizacion;
+            }
+            result[i] = value_to_reach - penalizacion;
+        }
+        return result;
+    }
+    public codificacion_entera convertir_a_codificacion(int[] ordinal){
+        BitSet b = new BitSet(ordinal.length);
+        b.clear();
+
+        codificacion_entera cod = new codificacion_entera(ordinal.length);
+        for(int i = 0; i < ordinal.length; ++i){
+            int idx = 0;
+            int suma = 0;
+            int value_to_reach = ordinal[i];
+            while((idx=b.nextSetBit(idx))<value_to_reach){
+                ++idx; ++suma;
+            }
+            cod.set_value(i, value_to_reach+suma);
+        }
+        return cod;
     }
 }
