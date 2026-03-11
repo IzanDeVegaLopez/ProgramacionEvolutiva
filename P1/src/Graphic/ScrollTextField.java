646721969368172ae7b92de2ea9f5d22df39d39d
Eric11195 <izandeve@ucm.es>
2026-03-11 20:10:22 +0100

    texto de colirinchis

--- P1/src/Graphic/ScrollTextField.java
+++ P1/src/Graphic/ScrollTextField.java
@@ -1,23 +1,52 @@
 package Graphic;
 
 import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import java.awt.*;
 
 public class ScrollTextField extends JScrollPane {
-    JTextArea my_text_area;
+    JTextPane my_text_area;
     public ScrollTextField(){
-        super(my_text_area = new JTextArea(""), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        my_text_area.setLineWrap(true);
+        super(my_text_area = new JTextPane(), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        my_text_area.setText("");
     }
-    public void cleat_text(){
+    public void clear_text(){
         my_text_area.setText("");
     }
     public void add_text(String s){
-        my_text_area.append(s);
+        add_text(s, Color.BLACK);
+    }
+    public void add_text(String s, Color c){
+        appendToPane(my_text_area, s, c);
     }
     public void add_text_ln(String s){
-        my_text_area.append(s+'\n');
+        add_text_ln(s, Color.BLACK);
+    }
+    public void add_text_ln(String s, Color c){
+        add_text(s+'\n', c);
+    }
+    public void set_text(String s, Color c){
+        clear_text();
+        add_text(s, c);
     }
     public void set_text(String s){
-        my_text_area.setText(s);
+        set_text(s, Color.BLACK);
+    }
+    //src: https://stackoverflow.com/questions/9650992/how-to-change-text-color-in-the-jtextarea
+    private void appendToPane(JTextPane tp, String msg, Color c)
+    {
+        StyleContext sc = StyleContext.getDefaultStyleContext();
+        AttributeSet aset = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, c);
+
+        aset = sc.addAttribute(aset, StyleConstants.FontFamily, "Lucida Console");
+        aset = sc.addAttribute(aset, StyleConstants.Alignment, StyleConstants.ALIGN_JUSTIFIED);
+
+        int len = tp.getDocument().getLength();
+        tp.setCaretPosition(len);
+        tp.setCharacterAttributes(aset, false);
+        tp.replaceSelection(msg);
     }
 }
