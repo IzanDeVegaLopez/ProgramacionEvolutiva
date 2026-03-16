31ff0b89c4a7b4ee009639544a86c79658737bed
Óscar Melquiades Durán Narganes <osduran@ucm.es>
2026-03-16 17:00:35 +0100

    Cruce múnico arreglado

--- P1/src/crossmethods/munic_cross.java
+++ P1/src/crossmethods/munic_cross.java
@@ -9,16 +9,30 @@ public class munic_cross implements base_cross_method{
         codificacion_entera victor = new codificacion_entera(size);
         codificacion_entera mictor = new codificacion_entera(size);
 
+        int idx1 = 0;
+        int idx2 = 0;
         for (int i = 0; i<element1.get_size();++i){
-            int id1 = element1.get_value(i);
-            int id2 = element2.get_value(i);
-            if (victor.is_contained(id1) || mictor.is_contained(id2)){
-                victor.set_value(i,id2);
-                mictor.set_value(i,id1);
-            }
-            else if (victor.is_contained(id2) || mictor.is_contained(id1)){
-                victor.set_value(i,id1);
-                mictor.set_value(i,id2);
+            int val1 = element1.get_value(i);
+            int val2 = element2.get_value(i);
+
+            boolean[] checks = new boolean[]{victor.is_contained(val1), mictor.is_contained(val1),
+                    victor.is_contained(val2), mictor.is_contained(val2)};
+            if (checks[0] || checks[1] || checks[2] || checks[3]) {
+                if (checks[0]) {
+                    mictor.set_value(idx2, val1);
+                    ++idx2;
+                } else {
+                    victor.set_value(idx1, val1);
+                    ++idx1;
+                }
+
+                if (checks[2]) {
+                    mictor.set_value(idx2, val2);
+                    ++idx2;
+                } else {
+                    victor.set_value(idx1, val2);
+                    ++idx1;
+                }
             }
             else{
                 int atk_idx = i - 1;
@@ -39,8 +53,10 @@ public class munic_cross implements base_cross_method{
                     }
                     first_attacking = !first_attacking;
                 }
-                victor.set_value(i,glad2.arm<0? id1 : id2 );
-                mictor.set_value(i,glad2.arm<0? id2 : id1 );
+                victor.set_value(idx1,glad2.arm<0? val1 : val2 );
+                mictor.set_value(idx2,glad2.arm<0? val2 : val1 );
+                ++idx1;
+                ++idx2;
             }
         }
         element1.copy(victor);
