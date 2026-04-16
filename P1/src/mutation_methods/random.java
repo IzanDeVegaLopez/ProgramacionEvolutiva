0e32f96319fa044449bd15672a18caf8bfc12127
Eric11195 <izandeve@ucm.es>
2026-04-16 23:25:22 +0200

    mutación random

--- P1/src/mutation_methods/random.java
+++ P1/src/mutation_methods/random.java
@@ -2,9 +2,17 @@ package mutation_methods;
 
 import codification.IndividualCodification;
 import Error.UnreachableCode;
+import utils.my_utils;
 
 public class random implements BaseMutation{
     public void mutate(IndividualCodification cod) throws Exception{
-        throw new UnreachableCode("Unimplemented");
+        int idx = my_utils.get_random(4);
+        switch(idx){
+            case 0 -> new terminal().mutate(cod);
+            case 1 -> new funcional().mutate(cod);
+            case 2 -> new subarbol().mutate(cod);
+            case 3 -> new hoist().mutate(cod);
+            default -> throw new UnreachableCode("Max value should be 4, and min 0. What happened? Result was:" + idx);
+        }
     }
 }
