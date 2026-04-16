6ad1fe283e4f9d3a4785d18f21a9419b53cd321c
Eric11195 <izandeve@ucm.es>
2026-04-16 23:21:47 +0200

    mutación hoist

--- P1/src/mutation_methods/hoist.java
+++ P1/src/mutation_methods/hoist.java
@@ -2,10 +2,13 @@ package mutation_methods;
 
 import codification.IndividualCodification;
 import Error.UnreachableCode;
+import codification.NodeFactory;
+import codification.TreeNode;
 
 //PODA
 public class hoist implements BaseMutation{
     public void mutate(IndividualCodification cod) throws Exception{
-        throw new UnreachableCode("Unimplemented");
+        TreeNode tn = NodeFactory.choose_random_node(cod.node_tree);
+        cod.node_tree = tn;
     }
 }
