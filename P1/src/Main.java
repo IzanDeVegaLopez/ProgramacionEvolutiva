35d8b6f0d1ce46f3767494e0f87bd78620811a99
Óscar Melquiades Durán Narganes <149335285+Melduna@users.noreply.github.com>
2026-04-17 14:32:59 +0200

    Ajustado para Java 24

--- P1/src/Main.java
+++ P1/src/Main.java
@@ -1,6 +1,7 @@
 import Graphic.MainMenu;
 
-
-void main() {
-    new MainMenu();
+public class Main {
+    public static void main(String[] args) {
+        new MainMenu();
+    }
 }
--- P1/src/codification/codificacion_entera.java
+++ P1/src/codification/codificacion_entera.java
@@ -35,13 +35,13 @@ public class codificacion_entera {
     public int get_free(){return free_values;}
     public void set_value(int index, int new_value){
         if(new_value >= conflict_point.length){
-            IO.print("Explotar");
+            System.out.print("Explotar");
         }
         if(values[index]!=-1){
-            IO.print("Se ha intentado asignar un elemento que ya estaba asignado");
+            System.out.print("Se ha intentado asignar un elemento que ya estaba asignado");
         }
         if(conflict_point[new_value]!=-1){
-            IO.print("Se ha intentado asignar un elemento que ya estaba asignado");
+            System.out.print("Se ha intentado asignar un elemento que ya estaba asignado");
         }
         values[index] = new_value;
         conflict_point[new_value]=index;
