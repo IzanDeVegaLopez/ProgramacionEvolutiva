1144f5bf8f5aaa3d74d1d73e3f09602c8728c222
Óscar Melquiades Durán Narganes <osduran@ucm.es>
2026-04-20 21:53:58 +0200

    Reapply "Mapas almacenados aparte"
    This reverts commit 7b8f9e7dfde8f00a41fcd7741e9a3ef803d90bbc.

--- P1/src/Mapas/mapStorage.java
+++ P1/src/Mapas/mapStorage.java
@@ -2,12 +2,19 @@ package Mapas;
 
 
 public class mapStorage {
+    // Map templates that should not be changed during runtime
+    private static Map[] map_blueprints = new Map[3];
+    // Maps that may be changed during runtime
     private static Map[] maps = new Map[3];
     public static void add_map(Map m, int n){
-        maps[n] = m;
+        map_blueprints[n] = m;
+        reset_maps();
     }
     public static Map get_map(int n){
         if (n>maps.length) return new Map(new TileContents[0][0]);
         return maps[n];
+    }
+    public static void reset_maps(){
+        System.arraycopy(map_blueprints, 0, maps, 0, 3);
     }
 }
